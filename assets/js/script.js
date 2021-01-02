bf427f33e6f520ddfae47fbc28b779c910204822
Russ <ocjade@comcast.net>
2021-01-02 12:19:08 -0700

    Added Form with Bootstrap Validation

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// Disable form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+    // Capture form to apply validation styles
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
